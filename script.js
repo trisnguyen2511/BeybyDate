196442a0321752eb0ee897aefc3293d11d7369f8
trisnguyen2511 <75766468+trisnguyen2511@users.noreply.github.com>
2025-01-25 23:34:11 +0700

    commit src index

---
+++ script.js
@@ -0,0 +1,36 @@
+let count = 0;
+
+document.getElementById('yes').addEventListener('click', () => {
+    alert('yes');
+});
+document.getElementById('no').addEventListener('click', () => {
+    const button = document.getElementById('no');
+    const buttonWidth = button.offsetWidth;
+    const buttonHeight = button.offsetHeight;
+    const randomX = Math.floor(Math.random() * (window.innerWidth - buttonWidth));
+    const randomY = Math.floor(Math.random() * (window.innerHeight - buttonHeight));
+    button.style.position = 'absolute';
+    button.style.left = `${randomX}px`;
+    button.style.top = `${randomY}px`;
+
+    //Xử lý hint
+    const hint = document.getElementById('hintNo');
+    switch (parseInt(count)) {
+        case 0:
+            hint.innerText = 'Đừng mò 🥺';
+            break;
+        case 1:
+            hint.innerText = 'Năn nỉ ó 😣';
+            break;
+        case 2:
+            hint.innerText = 'Hứa sẽ ngoan 😇';
+            break;
+        case 3:
+            hint.innerText = 'TÔI KHÔNG CHO PHÉP EM BẤM "HONG" 😡';
+            break;
+        default:
+            hint.innerText = 'Giận😡';
+    }
+    count++;
+});
+
